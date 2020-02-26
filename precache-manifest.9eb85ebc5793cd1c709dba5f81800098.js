1c32df813c1466fe79cb12057633d3dec83346ec
Marco Whyte <mwhyte@umass.edu>
2020-02-26 10:57:57 -0600

    Updates

---
+++ precache-manifest.9eb85ebc5793cd1c709dba5f81800098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b550ba8435c9ba9d6a0fbcd7ffbccb41",
+    "url": "/portfolioWebsite/index.html"
+  },
+  {
+    "revision": "3d222de735a8f3e46470",
+    "url": "/portfolioWebsite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/portfolioWebsite/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolioWebsite/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d222de735a8f3e46470",
+    "url": "/portfolioWebsite/static/js/main.60f458c7.chunk.js"
+  },
+  {
+    "revision": "b6af1f83d3a6e75385cc",
+    "url": "/portfolioWebsite/static/js/runtime-main.bc934824.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolioWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
