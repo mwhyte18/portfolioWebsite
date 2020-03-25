80f41d91929e53271135307944f07b792f3c17a5
Marco Whyte <mwhyte@umass.edu>
2020-03-24 22:18:56 -0500

    finished work section layout, css, footer

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,65 +1,143 @@
 import React from "react";
 import modernizeme from "./modernizeme.PNG";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faClipboardList } from "@fortawesome/free-solid-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faSpaceShuttle } from "@fortawesome/free-solid-svg-icons";
+
 const Work = () => {
   return (
-    <div id="work">
+    <div className="work" id="work">
       <h1>My Work</h1>
-      <div className="myWork">
-        <div className="task">
-          <img src={modernizeme} alt="Mountains" class="task-layer_bottom" />
-          <div className="task-layer_top">
-            <div className="task-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              porttitor ligula porttitor, lacinia sapien non.
-              <br></br>
-              <button>Click here</button>
-            </div>
-          </div>
-        </div>
-        <div className="task">
-          <img
-            src={modernizeme}
-            alt="Mountains"
-            className="task-layer_bottom"
+      <div className="task">
+        <div
+          onClick={() => window.open("http://www.google.com", "_blank")}
+          className="nameAndIcon"
+        >
+          <FontAwesomeIcon
+            className="clipboard"
+            icon={faClipboardList}
+            size="2x"
           />
-          <div class="task-layer_top">
-            <div class="task-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              porttitor ligula porttitor, lacinia sapien non.
-              <br></br>
-              <button>Click here</button>
-            </div>
-          </div>
+
+          <h2>Todo App</h2>
         </div>
-        <div className="task">
-          <img
-            src={modernizeme}
-            alt="Mountains"
-            className="task-layer_bottom"
-          />
-          <div className="task-layer_top">
-            <div className="task-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              porttitor ligula porttitor, lacinia sapien non.
-              <br></br>
-              <button>Click here</button>
-            </div>
-          </div>
+        <p>
+          A simple login todo app, making all your tasks easily accessible. Uses
+          react-redux and node.js
+        </p>
+        <div className="icons">
+          <button className="github">
+            <FontAwesomeIcon icon={faGithub} />
+            {"  "}
+            Open Source
+          </button>
+          <button className="visitSite">Visit Site</button>
         </div>
-        <div className="task">
-          <img
-            src={modernizeme}
-            alt="Mountains"
-            className="task-layer_bottom"
+      </div>
+      <div className="task">
+        <div
+          onClick={() => window.open("http://www.google.com", "_blank")}
+          className="nameAndIcon"
+        >
+          <FontAwesomeIcon
+            className="faLaptopCode"
+            icon={faLaptopCode}
+            size="2x"
           />
-          <div className="task-layer_top">
-            <div className="task-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              porttitor ligula porttitor, lacinia sapien non.
-              <br></br>
-              <button>Click here</button>
-            </div>
-          </div>
+
+          <h2>Modernize Me</h2>
+        </div>
+        <p>
+          A simple login todo app, using redux and login with backend asd asdfa
+          ja j sdjo aojd jasdoj
+        </p>
+        <div className="icons">
+          <button className="github">
+            <FontAwesomeIcon icon={faGithub} />
+            {"  "}
+            Open Source
+          </button>
+          <button className="visitSite">Visit Site</button>
+        </div>
+      </div>
+      <div className="task">
+        <div
+          onClick={() => window.open("http://www.google.com", "_blank")}
+          className="nameAndIcon"
+        >
+          <FontAwesomeIcon
+            className="faSpaceShuttle"
+            icon={faSpaceShuttle}
+            size="2x"
+          />
+
+          <h2>NASA Photo</h2>
+        </div>
+        <p>
+          A simple login todo app, using redux and login with backend asd asdfa
+          ja j sdjo aojd jasdoj
+        </p>
+        <div className="icons">
+          <button className="github">
+            <FontAwesomeIcon icon={faGithub} />
+            {"  "}
+            Open Source
+          </button>
+          <button className="visitSite">Visit Site</button>
+        </div>
+      </div>
+      <div className="task">
+        <div
+          onClick={() => window.open("http://www.google.com", "_blank")}
+          className="nameAndIcon"
+        >
+          <FontAwesomeIcon
+            className="faSpaceShuttle"
+            icon={faSpaceShuttle}
+            size="2x"
+          />
+
+          <h2>NASA Photo</h2>
+        </div>
+        <p>
+          A simple login todo app, using redux and login with backend asd asdfa
+          ja j sdjo aojd jasdoj
+        </p>
+        <div className="icons">
+          <button className="github">
+            <FontAwesomeIcon icon={faGithub} />
+            {"  "}
+            Open Source
+          </button>
+          <button className="visitSite">Visit Site</button>
+        </div>
+      </div>
+      <div className="task">
+        <div
+          onClick={() => window.open("http://www.google.com", "_blank")}
+          className="nameAndIcon"
+        >
+          <FontAwesomeIcon
+            className="faSpaceShuttle"
+            icon={faSpaceShuttle}
+            size="2x"
+          />
+
+          <h2>NASA Photo</h2>
+        </div>
+        <p>
+          A simple login todo app, using redux and login with backend asd asdfa
+          ja j sdjo aojd jasdoj
+        </p>
+        <div className="icons">
+          <button className="github">
+            <FontAwesomeIcon icon={faGithub} />
+            {"  "}
+            Open Source
+          </button>
+          <button className="visitSite">Visit Site</button>
         </div>
       </div>
     </div>
